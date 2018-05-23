395bf7c50ed2f3a878860c0ff121dbfffcbfd5bb
kopmoe <kop.still@gmail.com>
2018-05-24 01:12:31 +0800

    gateway

--- peach-service/peach-service-meituan/src/main/java/com/kopever/peach/service/meituan/MeituanApplication.java
+++ peach-service/peach-service-meituan/src/main/java/com/kopever/peach/service/meituan/MeituanApplication.java
@@ -2,8 +2,10 @@ package com.kopever.peach.service.meituan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MeituanApplication {
 
     public static void main(String[] args) {
